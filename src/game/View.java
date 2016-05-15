85d5e2c22d1c475b936639a78525648c535bc39f
haninsect <haninsect@gmail.com>
2016-05-15 22:02:47 +0800

    Add files via upload
    View的基本架構

--- src/game/View.java
+++ src/game/View.java
@@ -1,5 +1,49 @@
 package game;
 
+import processing.core.PApplet;
+import processing.core.PImage;
+import processing.data.JSONArray;
+import processing.data.JSONObject;
+
 public class View {
+	private Map map;
+	private Player player;
+	PApplet mainapplet;
+	JSONObject data;
+	JSONArray character, mapdata;
+	
+	PImage mapImage;
+	
+	public View (PApplet mainapplet, Map map, Player player) {
+		this.mainapplet = mainapplet;
+		this.map =  map;
+		this.player = player;
+	}
+	
+	public void display(JSONObject data){
+		this.data = data;
+		//Get other players's position from JSON file.
+		//Draw map image.
+		/*
+		 mapImage = map.getSubMap(player.getX, player.getY);
+		 mainapplet.image(mapImage, 0, 0);
+		 
+		 */
+		//Draw my player.
+		/*
+		 
+		 
+		 */
+		//Draw other players if I can see it.
+		/*
+		 
+		 
+		 */
+		//Draw a circle field of view. 
+		/*
+		 
+		 
+		 */
+	}
 
 }
