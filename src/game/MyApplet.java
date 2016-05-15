e3a0a672711858fa87fa96cabd091fd7e70c9136
u10000129 <u10000129@gmail.com>
2016-05-15 22:59:15 +0800

    fix move speed
    fix move time is same no matter the distance is different.

--- src/game/MyApplet.java
+++ src/game/MyApplet.java
@@ -6,22 +6,30 @@ import processing.core.PApplet;
 @SuppressWarnings("serial")
 public class MyApplet extends PApplet{
 	private Player player;
+	//private View view;
+	//private Map map;
+	public final static double speed = 0.004;
 	public final static int width = 800, height = 600;
 	
 	public void setup(){
 		size(width, height);
 		Ani.init(this);
 		player = new Player(this);
+		//map = new Map(this);
+		//view = new View(this, map, player);
 		smooth();
-		}
+	}
 	
 	public void draw(){
 		background(255);
 		player.display();
+		//view.display();
 	}
 	
 	public void mouseClicked(){
-		Ani.to(player, (float)0.5, "curX", mouseX);
-		Ani.to(player, (float)0.5, "curY", mouseY);
+		Ani.to(player, (float)(speed*dist(player.curX, player.curY, mouseX, mouseY)), 
+				"curX", mouseX, Ani.SINE_IN_OUT);
+		Ani.to(player, (float)(speed*dist(player.curX, player.curY, mouseX, mouseY)), 
+				"curY", mouseY, Ani.SINE_IN_OUT);
 	}
 }
