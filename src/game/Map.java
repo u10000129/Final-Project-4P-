772343d5d9aeb15ae36ff5acaa9292cdacc54e5f
Ted Yu <tedio85@yahoo.com.tw>
2016-05-18 14:17:07 +0800

    map fixed

--- src/game/Map.java
+++ src/game/Map.java
@@ -16,7 +16,7 @@ public class Map {
 		
 	}
 	
-	public Bounds horizontalWall() {
+	public Bounds horizontalWall(int x, int y) {
 		if(x+MyApplet.width/2 >= map.width)
 			return Bounds.RIGHT;
 		else if(x-MyApplet.width/2 < 0) 
@@ -25,7 +25,7 @@ public class Map {
 			return Bounds.NONE;
 	}
 	
-	public Bounds verticalWall() {
+	public Bounds verticalWall(int x, int y) {
 		if(y+MyApplet.height/2 >= map.height)
 			return Bounds.DOWN;
 		else if(y-MyApplet.height/2 < 0 )
@@ -40,15 +40,15 @@ public class Map {
 	
 	public PImage getSubMap(int x, int y) {
 		int X=0, Y=0;
-		if(horizontalWall() == Bounds.RIGHT)
+		if(horizontalWall(x,y) == Bounds.RIGHT)
 			X = map.width - MyApplet.width - 1;
-		else if(horizontalWall() == Bounds.LEFT)
+		else if(horizontalWall(x,y) == Bounds.LEFT)
 			X = 0;
 		else 
 			X = x - MyApplet.width/2;
-		if(verticalWall() == Bounds.DOWN)
+		if(verticalWall(x,y) == Bounds.DOWN)
 			Y = map.height - MyApplet.height -1 ;
-		else if(verticalWall() == Bounds.UP)
+		else if(verticalWall(x,y) == Bounds.UP)
 			Y = 0;
 		else
 			Y = y - MyApplet.height;
@@ -63,20 +63,21 @@ public class Map {
 		int[][] ret = new int[MyApplet.width][MyApplet.height];
 		int initRow = 0;
 		int from = 0;
-		if(verticalWall() == Bounds.UP)
+		if(verticalWall(x,y) == Bounds.UP)
 			initRow = 0;
-		else if(verticalWall() == Bounds.DOWN)
+		else if(verticalWall(x,y) == Bounds.DOWN)
 			initRow = map.height - MyApplet.height - 1;
 		else
 			initRow = y - MyApplet.height/2;
-		if(horizontalWall() == Bounds.LEFT)
+		if(horizontalWall(x,y) == Bounds.LEFT)
 			from = 0;
-		else if(horizontalWall() == Bounds.RIGHT)
+		else if(horizontalWall(x,y) == Bounds.RIGHT)
 			from = map.width - MyApplet.width - 1;
 		else
 			from = x - MyApplet.width/2;
 		for(int i=initRow;i<MyApplet.height;i++) {
-			ret[i] = Arrays.copyOfRange(map[i+initRow], from, from + MyApplet.width);
+			ret[i] = Arrays.copyOfRange(collisionMap[i+initRow], from, from + MyApplet.width);
 		}
+		return ret;
 	}
 }
