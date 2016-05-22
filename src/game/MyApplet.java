60081f1951c4b6375f150694b76351224d189700
haninsect <haninsect@gmail.com>
2016-05-22 21:24:41 +0800

    Update MyApplet.java

--- src/game/MyApplet.java
+++ src/game/MyApplet.java
@@ -10,6 +10,8 @@ public class MyApplet extends PApplet{
 	private Map map;
 	public final static double speed = 0.004;
 	public final static int width = 800, height = 600;
+	private final int moveUnit = 800;
+	
 	
 	public void setup(){
 		size(width, height);
@@ -29,45 +31,45 @@ public class MyApplet extends PApplet{
 	public void mousePressed(){
 		Bounds hBound = map.horizontalWall(player.getX(), player.getY());
 		Bounds vBound = map.verticalWall(player.getX(), player.getY());
-		int moveX, moveY;
+		int moveX, moveY;		
 		
 		//HorizonBound detect
 		if(hBound == Bounds.LEFT){ // At Left bounds.
 			moveX = mouseX;
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, player.curY)), 
-					//"curX", moveX, Ani.SINE_IN_OUT);
 		}
 		else if (hBound == Bounds.RIGHT){ // At Right bounds.
 			moveX = map.getFullMap().width - MyApplet.width +  mouseX;
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, player.curY)), 
-					//"curX", moveX, Ani.SINE_IN_OUT);
 		}
 		else {
 			moveX = player.getX() + mouseX - MyApplet.width/2 ;
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, player.curY)), 
-					//"curX", moveX, Ani.SINE_IN_OUT);
 		}
 		
 		//verticalBound detect
 		if(vBound == Bounds.UP){ // At Up bounds.
 			moveY = mouseY;
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, player.curX, moveY)), 
-					//"curY", moveY, Ani.SINE_IN_OUT);
 		}
 		else if (vBound == Bounds.DOWN){ // At Down bounds.
 			moveY = map.getFullMap().height - MyApplet.height + mouseY;
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, player.curX, moveY)), 
-					//"curY", moveY, Ani.SINE_IN_OUT);
 		}
 		else {
-			moveY = player.getY()+ mouseY - MyApplet.height/2;		
-			//Ani.to(player, (float)(speed*dist(player.curX, player.curY, player.curX, moveY)), 
-					//"curY", moveY, Ani.SINE_IN_OUT);
+			moveY = player.getY()+ mouseY - MyApplet.height/2;	
+		}		
+		
+		
+		// 
+		float partX = (float)( moveX - player.getX() ) /(float) moveUnit,
+				partY = (float)( moveY - player.getY() ) /(float) moveUnit;
+		int originalX = player.getX(),
+				originalY = player.getY();		
+		for(int i = 1; i <= moveUnit; i++){
+			moveX = originalX + (int)( partX * i );
+			moveY = originalY + (int)( partY * i );
+			System.out.println(i +" : " +(int)(partX * i) + ", " + partY + "moveX&moveY" + moveX +", " + moveY);
+			Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, moveY)), 
+					"curX", moveX, Ani.SINE_IN_OUT);
+			Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, moveY)), 
+					"curY", moveY, Ani.SINE_IN_OUT);
 		}
-		Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, moveY)), 
-				"curX", moveX, Ani.SINE_IN_OUT);
-		Ani.to(player, (float)(speed*dist(player.curX, player.curY, moveX, moveY)), 
-				"curY", moveY, Ani.SINE_IN_OUT);
 		
 		
 	}
