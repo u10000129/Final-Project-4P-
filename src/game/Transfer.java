0383d847931ae62d51e8a1ca9bf1e5a2ab1c0669
Ted Yu <tedio85@yahoo.com.tw>
2016-05-24 22:53:47 +0800

    Transfer complete but still need testing

--- src/game/Transfer.java
+++ src/game/Transfer.java
@@ -6,12 +6,13 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.io.PrintWriter;
 import java.net.Socket;
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map.Entry;
 
 import processing.data.JSONArray;
 import processing.data.JSONObject;
-
 
 
 public class Transfer {
@@ -21,6 +22,7 @@ public class Transfer {
 	private String line;
 	private PrintWriter writer;
 	private boolean gameStart;
+	private JSONObject json;
 	
 	public Transfer(String IP, int portNum) {
 		socket = null;
@@ -38,6 +40,7 @@ public class Transfer {
 					reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 					while(true) {
 						line = reader.readLine();
+						json = JSONObject.parse(line);
 					}
 				}
 				catch(IOException e) {
@@ -67,20 +70,24 @@ public class Transfer {
 		}
 	}
 	
-	public void setGameStatus(Boolean b) {
-		gameStart = b;
-	}
-	
 	public void encode(String name, 
 										int characterX, int characterY,
 										java.util.Map<Integer, List<Integer>> jewels,
 										java.util.Map <Integer, List<Integer>>hunters) {
+		
 		JSONObject obj = new JSONObject();
 		obj.setLong("time",System.currentTimeMillis());
-		obj.setString("name", name);
-		obj.setString("characterX", String.valueOf(characterX));
-		obj.setString("characterY", String.valueOf(characterY));
 		
+		// player array
+		JSONArray playerArray = new JSONArray();
+		JSONObject player = new JSONObject();
+		player.setString("name", name);
+		player.setString("characterX", String.valueOf(characterX));
+		player.setString("characterY", String.valueOf(characterY));
+		playerArray.setJSONObject(0, player);
+		obj.setJSONArray("players", playerArray);
+		
+		// jewel array
 		JSONArray jewelArray =  new JSONArray();
 		int count = 0;
 		for(Entry<Integer, List<Integer>> jewel : jewels.entrySet()) {
@@ -101,6 +108,7 @@ public class Transfer {
 		}
 		obj.setJSONArray("jewels", jewelArray);
 		
+		//hunter array
 		JSONArray hunterArray = new JSONArray();
 		count = 0;
 		for(Entry<Integer, List<Integer>> hunter : hunters.entrySet()) {
@@ -121,26 +129,70 @@ public class Transfer {
 		}
 		obj.setJSONArray("hunters", hunterArray);
 		
+		writer.println(obj.toString());
+		writer.flush();
 	}
 	
 	public Boolean getGameStatus() {
 		
+		gameStart =json.getBoolean("status");
+		return gameStart;
 	}
 	
 	public Long getTime() {
 		
+		return json.getLong("time");
 	}
 	
-	public Map getPlayers() { // returns a Map<int, List<int>>, which is ID->(x,y)
+	public java.util.Map<Integer, List<Integer>> getPlayers() { // Map of  ID->(x,y)
 		
+		JSONArray playerArray = json.getJSONArray("players");
+		
+		java.util.Map<Integer, List<Integer>> players = new HashMap<Integer, List<Integer>>();
+		for(int i=0;i<playerArray.size();i++) {
+			
+			List<Integer> list = new ArrayList<Integer>();
+			list.add(json.getInt("x"));
+			list.add(json.getInt("y"));
+			
+			players.put(json.getInt("id"), list);
+		}
+		
+		return players;
 	}
 	
-	public Map getHunters() { // returns a Map<int, List<int>>, which is ID->(x,y)
+	public java.util.Map<Integer, List<Integer>>   getHunters() { // Map of ID->(x,y)
 		
+		JSONArray hunterArray = json.getJSONArray("hunters");
+		
+		java.util.Map<Integer, List<Integer>> hunters = new HashMap<Integer, List<Integer>>();
+		for(int i=0;i<hunterArray.size();i++) {
+			
+			List<Integer> list = new ArrayList<Integer>();
+			list.add(json.getInt("x"));
+			list.add(json.getInt("y"));
+			
+			hunters.put(json.getInt("id"), list);
+		}
+		
+		return hunters;
 	}
 	
-	public Map getJewel() {		// returns a Map<int, List<int>>,  which is  ID->(x,y)
+	public java.util.Map<Integer, List<Integer>> getJewel() {		// Map of  ID->(x,y)
 		
+		JSONArray jewelArray = json.getJSONArray("jewels");
+		
+		java.util.Map<Integer, List<Integer>> jewels = new HashMap<Integer, List<Integer>>();
+		for(int i=0;i<jewelArray.size();i++) {
+			
+			List<Integer> list = new ArrayList<Integer>();
+			list.add(json.getInt("x"));
+			list.add(json.getInt("y"));
+			
+			jewels.put(json.getInt("id"), list);
+		}
+		
+		return jewels;
 	}
 	
 }
